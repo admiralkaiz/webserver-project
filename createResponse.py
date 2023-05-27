846db6a632d044e485f263c7aa00d749a9c11615
0xkaizaa <91956750+kaizaadesuka@users.noreply.github.com>
2023-05-27 16:10:51 +0700

    Update createResponse.py

--- createResponse.py
+++ createResponse.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 ##################################################
 # Filename: createResponse.py                    #
 ##################################################
@@ -14,12 +12,29 @@ def createResponse(response_code, response_body):
     # yang akan dikirimkan
     # Fungsi mengembalikan string respons berupa header dan body.
 
+    # Header respons HTTP dalam bentuk dictionary. 
+    # Sejauh ini server hanya dapat membalas dengan Content-Type
+    # berupa text/html.
     response_headers = {
         'Content-Type': 'text/html; charset=utf-8',
         'Content-Length': str(len(response_body)),
         'Connection': 'close',
     }
 
+    # Menggabungkan dictionary response_headers menjadi satu string tunggal.
     response_headers_raw = ''.join(f'{k}: {v}\r\n' for k, v in response_headers.items())
 
-    return f'HTTP/1.1 {str(response_code)}\r\n{response_headers_raw}\r\n{response_body}'
+    # Pesan HTTP response status berdasarkan kode response_code
+    # response_code = 200 -> HTTP/1.1 200 OK
+    # response_code = 400 -> HTTP/1.1 400 Bad Request
+    # response_code = 404 -> HTTP/1.1 404 Not Found
+    if response_code==200:
+        response_status = str(response_code) + " OK"
+    elif response_code==400:
+        response_status = str(response_code) + " Bad Request"
+    elif response_code==404:
+        response_status = str(response_code) + " Not Found"
+
+    # Mengembalikan string tunggal dari penggabungan antara
+    # response_status, response_headers_raw, dan response_body
+    return f'HTTP/1.1 {response_status}\r\n{response_headers_raw}\r\n{response_body}'
